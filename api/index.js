70c5392bb627849ce6221de0f04729f3b0203b30
sa-borgia3 <sa.borgia3@gmail.com>
2022-04-01 22:34:12 +0200

    session express and route static react

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const sequelize = require("./config/database");
+const path = require("path");
 const User = require("./models/user");
 const Tweet = require("./models/tweet");
 const cors = require("cors");
+var session = require("express-session");
 
 const whitelist = ["http://localhost:3000"];
 const corsOptions = {
@@ -22,12 +23,26 @@ app.use(cors(corsOptions));
 //Routes
 const userRoutes = require("./routes/user");
 const tweetRoutes = require("./routes/tweet");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  "/static",
+  express.static(path.join(__dirname, "../client/build//static"))
+);
 
 app.get("/", (req, res, next) => {
   res.send("Heello this is Index, please use Postman to test this REST_API !!");
 });
+
+app.use(
+  session({
+    secret: "mysecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(userRoutes);
 app.use(tweetRoutes);
@@ -38,7 +53,11 @@ Tweet.belongsTo(User, { foreignKey: "user_id" });
 
 Tweet.sequelize.sync();
 User.sequelize.sync();
-
+app.get("*", function (req, res) {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../../client/build/"),
+  });
+});
 app.listen(3001, () => {
   console.log(`Server listening on `);
 });
